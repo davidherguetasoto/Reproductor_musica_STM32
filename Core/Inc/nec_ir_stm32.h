cedf9e6db45a17459bcb4b0b5bfd809b83d55f2f
davidherguetasoto <david.hergueta.soto@alumnos.upm.es>
2022-12-17 14:29:21 +0100

    Corregido error encapsulamiento libreria NEC

--- Core/Inc/nec_ir_stm32.h
+++ Core/Inc/nec_ir_stm32.h
@@ -17,7 +17,7 @@
  */
 #ifndef SRC_IR_DETECTOR_STM32_H_
 #define SRC_IR_DETECTOR_STM32_H_
-#include"main.h"
+#include<stdint.h>
 void getDataIR(uint32_t* buffer, uint32_t time, uint8_t* END); //Función encargada de la decodificación de la señal infrarroja
 void decodeIR(uint32_t* buffer, uint8_t* device_code, uint8_t* data); //Función encargada de obtener el código del emisor y el mensaje a partir de la información recibida
 void getButton(uint8_t data, char* texto); //Devuelve el nombre de la tecla presionada a partir del código recibido
